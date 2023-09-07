72ab93b8d76716995f6fa5cee7183a418e1250c6
Vivek-Maltare <62413164+Vivek-Maltare@users.noreply.github.com>
2023-09-07 21:31:12 +0530

    Update program_5_handsonlist.c

--- program_5_handsonlist.c
+++ program_5_handsonlist.c
@@ -4,10 +4,13 @@
 int main(void)
 {
 int fd1,fd2,fd3,fd4,fd5;
-fd1=open("h1",O_RDWR);
-fd2=open("h2",O_RDWR);
-fd3=open("h3",O_RDWR);
-fd4=open("h4",O_RDWR);
-fd5=open("h5",O_RDWR);
-for(;;);
+fd1=open("h1",O_RDWR|O_CREAT);
+fd2=open("h2",O_RDWR|O_CREAT);
+fd3=open("h3",O_RDWR|O_CREAT);
+fd4=open("h4",O_RDWR|O_CREAT);
+fd5=open("h5",O_RDWR|O_CREAT);
+while(1)
+  {
+    
+  }
 }
