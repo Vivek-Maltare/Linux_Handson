7127cbb651c87c5136d9d7450fe5105f55cef1b1
Vivek-Maltare <62413164+Vivek-Maltare@users.noreply.github.com>
2023-09-10 00:17:18 +0530

    Update program_7_handsonlist.c
    updated program 7

--- program_7_handsonlist.c
+++ program_7_handsonlist.c
@@ -14,19 +14,21 @@ if(argc!=3)
 }
 int fd_read=open(argv[1],O_RDONLY);
 int fd_write=open(argv[2],O_WRONLY|O_CREAT);
-if(fd_read || fd_write == -1)
+if(fd_read == -1 || fd_write == -1)
     printf("Useless");
 while(1)
 {
 char buff;
 int char_read=read(fd_read,&buff,1);
-if(char_read==-1)
+if(char_read==0)
     break;
+if(char_read==-1)
+    break;    
 int char_written=write(fd_write,&buff,1);
 }
 int fd_read_close=close(fd_read);
 int fd_write_close=close(fd_write);
-if(fd_read_close || fd_write_close == -1)
+if(fd_read_close == -1 || fd_write_close == -1)
    printf("Useless:could not close");
 return 0;
 }
